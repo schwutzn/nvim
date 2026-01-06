7ab840eb7157d8e3f3a756f6fbb07b33ddeabf2f
Lukas <github@schwutzn.de>
2026-01-06 15:22:26 +0100

    remove comments

--- init.lua
+++ init.lua
@@ -629,7 +629,7 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'stylua',
         'prettier',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -683,10 +683,6 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettier' },
         typescript = { 'prettier' },
       },
